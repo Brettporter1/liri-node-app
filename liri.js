2b7b08c925810c3dc77d6a920ed9f756c2edd919
Brett Porter <gkdesign06@gmail.com>
2019-11-10 15:47:40 -0600

    added in final part of functionality

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ const keys = require('./keys.js');
 const Spotify = require('node-spotify-api');
 const axios = require('axios')
 const fs = require('fs')
+const moment = require('moment')
 
 const searchType = process.argv[2];
 const searchTerm = process.argv.slice(3).join(' ')
@@ -34,7 +35,13 @@ const concertThis = (band) =>{
     let concertURL = `https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`
 
     axios.get(concertURL).then(function(response){
-        console.log(response);
+        let concerts = response.data
+        for(let i = 0;i < concerts.length;i++)
+        console.log(`Venue: ${concerts[i].venue.name}
+Location: ${concerts[i].city}, ${concerts[i].region}
+Date: ${moment(concerts[i].datetime).format('MM/DD/YYYY')}
+_________________________________
+        `);
     })
 }
 
@@ -42,7 +49,7 @@ const concertThis = (band) =>{
 const artistName = (artist) => {return artist.name}
 const spotThis = (songSearch) => {
     if (!songSearch) {
-        songSearch = 'The Sign';
+        songSearch = '"The Sign"';
     }
     let spotify = new Spotify(keys.spotify)
     spotify.search({ type: 'track', query: songSearch }, function (err, data) {
@@ -51,7 +58,7 @@ const spotThis = (songSearch) => {
         } else {
             let songs = data.tracks.items
             for (let i = 0; i < songs.length; i++) {
-                console.log(`
+                console.log(`${parseInt([i])+1}
 Artist: ${songs[i].artists.map(artistName)}
 Song: ${songs[i].name}
 Preview: ${songs[i].preview_url}
@@ -65,19 +72,20 @@ _______________________________
 }
 // ==============================
 
-// OMDB Functon
+// OMDB Functon 
 const movieThis = (movie) => {
     let movieURL = `http://www.omdbapi.com/?t=${movie}&y=&plot=short&tomatoes=true&apikey=trilogy`;
     axios.get(movieURL).then(
     function(response) {
     let newMovie = response.data
+    // tomatoes=true within the api url doesn't actually give the Rotten Tomatoes rating, had to pull it from the Ratings key
     console.log(`
 ____________________________
 
 Title: ${newMovie.Title}
 Year: ${newMovie.Year}
 IMDB Rating: ${newMovie.imdbRating}
-Rotten Tomatoes Rating:${newMovie.tomatoRating}
+Rotten Tomatoes Rating:${newMovie.Ratings[1].Value}
 Country: ${newMovie.Country}
 Language: ${newMovie.Language}
 Plot: ${newMovie.Plot}
@@ -89,8 +97,7 @@ ____________________________
 }
 const checkMovie = (movie) => {
     if(!movie){
-        movie = 'Mr. Nobody'
-        movieThis(movie)
+        movieThis('Mr. Nobody')
         console.log(`
 If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
 
